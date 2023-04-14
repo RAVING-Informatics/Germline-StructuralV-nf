372d6474a9bcb0a3dceac12feff29d840f587e33
Gavin Monahan <gmonahan@setonix-03.pawsey.org.au>
2023-04-14 14:40:24 +0800

    add SVsize flag

--- run_SV_pipeline.sh
+++ run_SV_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l 
-#SBATCH --job-name=batch1
+#SBATCH --job-name=fix_annotsv
 #SBATCH --account=pawsey0848
 #SBATCH --partition=work
 #SBATCH --ntasks=1
@@ -20,4 +20,4 @@ module load nextflow/22.04.3
 conda activate graphviz
 
 #Run the pipeline
-nextflow run /software/projects/pawsey0848/sv/Germline-StructuralV-nf/main.nf --input /scratch/pawsey0848/sv/batch1/samples.tsv --ref /software/projects/pawsey0848/sv/references/hg38_masked/Homo_sapiens_assembly38_masked.fasta --intervals /software/projects/pawsey0848/sv/references/hg38_regions.bed.gz -config /software/projects/pawsey0848/sv/Germline-StructuralV-nf/config/setonix.config --annotsv /software/projects/pawsey0848/sv/references/
+nextflow run /software/projects/pawsey0848/sv/Germline-StructuralV-nf/main.nf --input /scratch/pawsey0848/sv/batch1/samples.tsv --ref /software/projects/pawsey0848/sv/references/hg38_masked/Homo_sapiens_assembly38_masked.fasta --intervals /software/projects/pawsey0848/sv/references/hg38_regions.bed.gz -config /software/projects/pawsey0848/sv/Germline-StructuralV-nf/config/setonix.config --annotsv /software/projects/pawsey0848/sv/references/ --minSVsize 39
